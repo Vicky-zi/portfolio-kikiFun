0d440d8e9ac0fc43781122b92168c6cbcdd29fd6
jarlinxx <80385174+Vicky-zi@users.noreply.github.com>
2022-06-29 16:40:20 +0800

    revise animate js

--- js/all.js
+++ js/all.js
@@ -8,35 +8,38 @@ $(document).ready(function() {
  
 
 	//滾軸效果
-	  //點選時發生 
+	//點選時發生 
   $('.scrollTop') .click(function(e){ 
     e.preventDefault(); 
    var target = $(this).attr('href'); //抓取選取的scrollTop id //
     var targetPos = $(target).offset().top; //用抓取到的id來判別位置// 
     $('body, html').animate({scrollTop:targetPos} ,1000);//用抓取到的位置來讓螢幕跑到定位 
   }); 
-    
-  // 抓取螢幕目前位置 
-  $(window).scroll(function(){ 
-    var scrollPos = $(window).scrollTop();  //抓取現在螢幕的寬度 //
-   
-   //滑到特定區域要加上的class 
-  $('.scrollTop').each(function(){ 
-    var target = $(this).attr('href'); //抓取選取的scrollTop id //
-    var targetPos = $(target).offset().top; //用抓取到的id來判別位置 //
-    var targetHeight = $(target).outerHeight();  //用抓取到的id來判別高度//
-    console.log( target , targetPos , targetHeight)
 
-    if(targetPos -1 <= scrollPos && (targetHeight + targetPos) > scrollPos) {
-      $('.scrollTop').removeClass('active'); 
-      $(this).addClass('active'); 
-    }else{ 
-      $(this).removeClass('active'); 
-    } 
-  }); 
-    
-  }); 
 	//滾軸效果 end
+
+
+
+  //動畫較果 start
+  $(window).scroll(function(){
+    let windowHeight = $(window).height();
+    let scrollPosition = $(window).scrollTop();
+
+    $('.animate').each(function(){
+      let thisPosition = $(this).offset().top;
+
+      //滑到特定區域要加上的class
+      if((windowHeight + scrollPosition) >= thisPosition) {
+        $(this).addClass('animate__fadeInDown');
+        $('.content>img').addClass('animate__fadeInLeft');
+      }else {
+        $(this).removeClass('animate__fadeInDown');
+      }
+    })
+  });
+
+   //動畫較果 end
+  
 
 
 	$('.moblie-link').on('click',function(e) {
