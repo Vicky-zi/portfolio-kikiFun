b5ba2a06a530517e9fd65cd2d7c7215b03d5011c
jarlinxx <80385174+Vicky-zi@users.noreply.github.com>
2022-07-25 01:11:35 +0800

    updata

--- js/all.js
+++ js/all.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
+
+  //navbar-moblie start
 	$('.showmenu').on('click',function(e) {
 		e.preventDefault();
 		$('body').toggleClass('menu-show');
 	});
-
-	// layout end
+	// navbar-moblie end
  
+
+
+  //index Page
 
 	//滾軸效果
 	//點選時發生 
@@ -15,13 +19,11 @@ $(document).ready(function() {
     var targetPos = $(target).offset().top; //用抓取到的id來判別位置// 
     $('body, html').animate({scrollTop:targetPos} ,1000);//用抓取到的位置來讓螢幕跑到定位 
   }); 
-
 	//滾軸效果 end
-
-
 
   //動畫較果 start
   $(window).scroll(function(){
+    
     let windowHeight = $(window).height();
     let scrollPosition = $(window).scrollTop();
 
@@ -38,10 +40,106 @@ $(document).ready(function() {
     })
   });
 
+
+  // 滑至頂端
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 200) {
+      if ($(".goTop").hasClass("hide")) {
+        $(".goTop").toggleClass("hide");
+      }
+    } else {
+      $(".goTop").addClass("hide");
+    }
+  });
+
+  $('.goTop').click(function () {
+    $('html,body').animate({ scrollTop: 0 }, 1000);
+});
+
    //動畫較果 end
+
+
+   
+   //表單驗證
+$('#form').validate({
+  onkeyup: function(element, event) {
+    //去除左側空白
+    var value = this.elementValue(element).replace(/^\s+/g, "");
+    $(element).val(value);
+   },
+   rules: {
+     fname: {
+       required: true
+     },
+     phone:{
+       required: true,
+       minlength: 8,
+       number: true
+     },
+     email: {
+       required: true,
+       email: true
+     },
+     password: {
+       required: true,
+       maxlength: 6,
+       number: true
+     }
+   },
+   messages: {
+     fname: {
+       required:'必填'
+     },
+     phone: {
+       required:'必填',
+       minlength:'不得少於8位',
+       number:'電話需為數字'
+     },
+     email: {
+       required:'必填',
+       email:'Email格式不正確'
+     },
+     password: {
+       required:'必填',
+     },
+   },
+   submitHandler: function(form) {
+     form.submit();
+   }
+ });  
+ });
+
+ // form end
+
+
+
+
   
 
 
+
+  //  Cart Page
+
+
+  //heart start
+	$('.btn').on('click',function(e) {
+		e.preventDefault();
+		$(this).toggleClass('heart-click');
+	});           
+	 // heart end
+
+
+// 加入購物車 start
+	$('.cart').on('click',  function(e){
+       e.preventDefault();
+       const numberElement = document.querySelector(".number");
+       const currentNumber = Number(numberElement.textContent);
+       numberElement.textContent = currentNumber + 1;
+});
+// 加入購物車 end
+
+
+  //moblie start
 	$('.moblie-link').on('click',function(e) {
 		e.preventDefault();
 		$('body').toggleClass('open');
@@ -50,83 +148,11 @@ $(document).ready(function() {
 		e.preventDefault();
 		$('body').toggleClass('clik-open');
 	});            
-	// t1 nav-top 顯示明細 end
+	// moblie end
 
 
-	$('.btn').on('click',function(e) {
-		e.preventDefault();
-		$(this).toggleClass('heart-click');
-	});           
-	 // heart 變換圖片 end
 
 
-// 購物車加(減)
-	$('.cart').on('click',  function(e){
-       e.preventDefault();
-       const numberElement = document.querySelector(".number");
-       const currentNumber = Number(numberElement.textContent);
-       numberElement.textContent = currentNumber + 1;
-});
-// 購物車加(減)
-
-//表單驗證
-$('#form').validate({
-  /* 常用檢測屬性
- required:必填
- noSpace:空白
- minlength:最小長度
- maxlength:最大長度
- email:信箱格式
- number:數字格式
- */
- onkeyup: function(element, event) {
-   //去除左側空白
-   var value = this.elementValue(element).replace(/^\s+/g, "");
-   $(element).val(value);
-  },
-  rules: {
-    fname: {
-      required: true
-    },
-    phone:{
-      required: true,
-      minlength: 8,
-      number: true
-    },
-    email: {
-      required: true,
-      email: true
-    },
-    password: {
-      required: true,
-      maxlength: 6,
-      number: true
-    }
-  },
-  messages: {
-    fname: {
-      required:'必填'
-    },
-    phone: {
-      required:'必填',
-      minlength:'不得少於8位',
-      number:'電話需為數字'
-    },
-    email: {
-      required:'必填',
-      email:'Email格式不正確'
-    },
-    password: {
-      required:'必填',
-    },
-  },
-  submitHandler: function(form) {
-    form.submit();
-  }
-});
-
-	
-});
 
 
 
